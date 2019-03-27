bd1e6f1bea49ad0911ad215a6c036ab8117587ca
= <douglaswgerhardt@gmail.com>
2019-03-27 09:29:57 -0400

    Added Lev's address

--- src/function-handlers/http/email/address-restriction.ts
+++ src/function-handlers/http/email/address-restriction.ts
@@ -4,7 +4,8 @@ export default {
         'Vynsane@gmail.com',
         'Vynsane@outlook.com',
         'cartustestsso111@gmail.com',
-        'cartustestingnotify@yahoo.com'
+        'cartustestingnotify@yahoo.com',
+        'lev.b.aks@gmail.com'
     ],
     allowedDomains: ['cartus.com']
 }
